d4955624fa512879d8d3c96025567c08965bc050
16046512 <16046512@myrp.edu.sg>
2018-05-22 10:02:33 +0800

    showsong

--- app/src/main/java/com/example/a16046512/p05problemstatement/ShowSong.java
+++ app/src/main/java/com/example/a16046512/p05problemstatement/ShowSong.java
@@ -14,18 +14,48 @@ import java.util.ArrayList;
 
 public class ShowSong extends AppCompatActivity {
 
+    private ListView songlist;
+    private Button btnfilter;
 
+    private SongAdapter sa;
+    private ArrayList<Song> song;
+    private DBHelper dbh = new DBHelper(ShowSong.this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_song);
+        songlist = (ListView)findViewById(R.id.songlist);
+        btnfilter = (Button)findViewById(R.id.btnfilter);
 
+        song = dbh.getAllSongs();
+        sa = new SongAdapter(ShowSong.this,R.layout.showsongrow,song);
+        songlist.setAdapter(sa);
+        songlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                Intent i = new Intent(ShowSong.this,ModifySong.class);
+                Song currentposition = song.get(pos);
+                startActivityForResult(i,9);
+            }
+        });
 
 
 
 
     }
 
-   
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 9){
+            song.clear();
+            DBHelper dbh = new DBHelper(this);
+            song.addAll(dbh.getAllSongs());
+            sa.notifyDataSetChanged();
+            Toast.makeText(ShowSong.this, "Update successful",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
 
 }
